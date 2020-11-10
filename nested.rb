900744465e36a81ef6acd15829b17de472306f4c
Elias Campos <ecampose76@gmail.com>
2020-11-10 00:03:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,7 +86,9 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto] = {""}
+programmer_hash = {
+  :yukihiro_matsumoto => {}
+}
 end
 
 def changing_alan
